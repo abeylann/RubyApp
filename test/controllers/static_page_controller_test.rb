63356c0d3d7c83c6445c21a42708a5d901615d88
abeylann <alicia.navarrete@deliveroo.co.uk>
2016-07-01 12:13:59 +0100

    ruby

--- test/controllers/static_page_controller_test.rb
+++ test/controllers/static_page_controller_test.rb
@@ -2,27 +2,27 @@ require 'test_helper'
 
 class StaticPageControllerTest < ActionDispatch::IntegrationTest
 
-def setup
-  @base_title = "Alicia Beylan"
-end
+# def setup
+#   @base_title = "Alicia Beylan"
+# end
 
   test "should get home" do
     get static_page_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    # assert_select "title", "Alicia Beylan"
   end
 
   test "should get help" do
     get static_page_help_url
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select "title", "Help | Alicia Beylan"
 
   end
 
   test "should get about" do
   	get static_page_about_url
   	assert_response :success    
-    assert_select "title", "About | #{@base_title}"
+    assert_select "title", "About | Alicia Beylan"
 
   end
 
